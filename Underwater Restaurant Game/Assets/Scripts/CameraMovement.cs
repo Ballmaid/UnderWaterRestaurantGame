a3dd0224cbe97097bc76bbf798856b3e9560897c
Leon Bartle <leon@bartle.eu>
2023-02-28 19:49:52 +0100

    Camera Motion, Menu input field, Second scene
    Added a second scene for Game, edited the Menu for multiplayer only, added a IP/Hostname input field.
    
    Created basic scene structure in Game scene, added two test tiles, added one test location, added movement controls for the camera.

---
+++ Underwater Restaurant Game/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform CameraTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+    }
+
+
+    void MoveCamera()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            CameraTransform.position += new Vector3(-0.05f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            CameraTransform.position += new Vector3(0.05f, 0, 0);
+        }
+    }
+}
