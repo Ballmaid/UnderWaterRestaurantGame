905eac5b4fbb083eca013b45b9f2ba3f34511136
Oleg Tolochko <89548801+Ballmaid@users.noreply.github.com>
2023-03-07 18:08:59 +0100

    Slider camera movement

--- Underwater Restaurant Game/Assets/Scripts/CameraMovement.cs
+++ Underwater Restaurant Game/Assets/Scripts/CameraMovement.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraMovement : MonoBehaviour
 {
     public Transform CameraTransform;
+    public Slider CameraSlider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CameraSlider.maxValue = 40;
     }
 
     // Update is called once per frame
@@ -17,16 +20,34 @@ public class CameraMovement : MonoBehaviour
         MoveCamera();
     }
 
-
+    public void SliderMove() {
+        CameraTransform.position = new Vector3(CameraSlider.value, CameraTransform.position.y, CameraTransform.position.z);
+    }
+    
     void MoveCamera()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            CameraTransform.position += new Vector3(-15f*Time.deltaTime, 0, 0);
+            if (CameraTransform.position.x > 0) {
+                CameraTransform.position += new Vector3(-15f*Time.deltaTime, 0, 0);
+                CameraSlider.value = CameraTransform.position.x;
+            }
+            else
+            {
+                CameraTransform.position = new Vector3(0, CameraTransform.position.y, CameraTransform.position.z);
+            }
+            
         }
         if (Input.GetKey(KeyCode.D))
         {
+            if (CameraTransform.position.x < CameraSlider.maxValue) {
             CameraTransform.position += new Vector3(15f*Time.deltaTime, 0, 0);
+            CameraSlider.value = CameraTransform.position.x;
+            }
+            else
+            {
+                CameraTransform.position = new Vector3(CameraSlider.maxValue, CameraTransform.position.y, CameraTransform.position.z);
+            }
         }
     }
 }
