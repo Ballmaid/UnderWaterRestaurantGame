05fafe58954937835a1776c2de155a79bea347cf
Leon Bartle <leon@bartle.eu>
2023-03-26 12:38:38 +0200

    Buffer Cleanup movement

--- Underwater Restaurant Game/Assets/Scripts/ItemBehavior.cs
+++ Underwater Restaurant Game/Assets/Scripts/ItemBehavior.cs
@@ -43,7 +43,27 @@ public class ItemBehavior : MonoBehaviour
         MoveItem((float)transform.position.x, (float)transform.position.y);
     }
     public void MoveItem(float posX, float posY){
-        networking.sendMessage(2, ItemID.ToString() + "," + ((int)(posX*10)).ToString() + "," +  ((int)(posY*10)).ToString());
+        //delete older message with same itemID
+        bool found = false;
+        string buffer = networking.buffer;
+        string[] bufferArray = buffer.Split(';');
+        for(int i = 0; i < bufferArray.Length; i++){
+            string[] message = bufferArray[i].Split(',');
+            if(message[0] == "2" && message[1] == ItemID.ToString()){
+                message[2] = ((int)(posX*10)).ToString();
+                message[3] = ((int)(posY*10)).ToString();
+                bufferArray[i] = message[0] + "," + message[1] + "," + message[2] + "," + message[3];
+                found = true;
+            }
+        }
+        networking.buffer = bufferArray[0];
+        for(int i = 1; i < bufferArray.Length; i++){
+            networking.buffer += ";" + bufferArray[i];
+        }
+        if(!found){
+            networking.sendMessage(2, ItemID.ToString() + "," + ((int)(posX*10)).ToString() + "," +  ((int)(posY*10)).ToString());
+        }
+        
         //Debug.Log("ItemID: " + ItemID + " moved to " + posX + ", " + posY);
     }
 }
